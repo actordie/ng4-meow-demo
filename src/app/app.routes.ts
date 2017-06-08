86364c73d6df9df47cf5c865d4efe387caddaa43
S JACKSON <jackson.sabastian@gmail.com>
2017-06-08 11:44:43 -0600

    added about template, route, component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 /* anytime u make a change to src directory, you need to rerun the build */
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home.component";
+import {AboutComponent} from "./components/about.component";
 
 
-export const allAppComponents = [HomeComponent];
+export const allAppComponents = [HomeComponent, AboutComponent];
 
 export const routes: Routes = [
+	{path: "about", component: AboutComponent},
 	{path: "", component: HomeComponent},
 	{path: "**", redirectTo: ""}
 ];
